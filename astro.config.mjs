a6a85b88b29f97e095cd7986dce1675a0e611022
oneilk10 <oneilk10@tcd.ie>
2025-08-04 23:45:54 +0100

    hey

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import { remarkKroki } from 'remark-kroki';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://blog.kaet.xyz',
+	integrations: [
+		mdx({
+			remarkPlugins: [
+				[remarkKroki, {
+					server: 'https://kroki.io',
+					output: 'inline-svg', // Changed from 'svg' to 'inline-svg'
+				}]
+			],
+		}), 
+		sitemap()
+	],
 });
